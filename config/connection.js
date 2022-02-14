af4d2fb8aaa65c70df1794925cef329b315449e5
Cesar Olguin <c3sarin3@gmail.com>
2022-02-14 01:35:37 -0500

    fixed some issues

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,6 @@ const sequelize = process.env.JAWSDB_URL ?
     new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: '127.0.0.1',
         dialect: 'mysql',
-        password: 'cesarin2001',
         dialectOptions: {
             decimalNumbers: true,
         },
